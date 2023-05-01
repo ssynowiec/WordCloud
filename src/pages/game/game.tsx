5cff8558e565add57537c60e3f0d94d629cdc201
Stanisław Synowiec <stasyn1410@gmail.com>
2023-05-01 12:50:52 +0200

    remove: comments

--- src/pages/game/game.tsx
+++ src/pages/game/game.tsx
@@ -3,22 +3,10 @@ import { MainLayout } from '../../components/layouts/mainLayout.tsx';
 import { GameForm } from '../../components/gameForm/gameForm.tsx';
 import { useGameForm } from '../../components/gameForm/useGameForm.tsx';
 
-// const drawQuestion = () => {
-// 	const questions = data;
-//
-// 	const randomIndex = Math.floor(Math.random() * questions.length);
-// 	const question = questions[randomIndex].question;
-// 	const answers = questions[randomIndex].all_words;
-// 	const correctAnswers = questions[randomIndex].good_words;
-//
-// 	return { question, answers, correctAnswers };
-// };
-
 export const Game = () => {
 	const {
 		question: { question, answers, correctAnswers },
 	} = useGameForm();
-	// const { question, answers, correctAnswers } = drawQuestion();
 
 	return (
 		<MainLayout>
