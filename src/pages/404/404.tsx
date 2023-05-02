720a6541f91b4bb07a0215d9da4e8c3dd28aed84
Stanisław Synowiec <stasyn1410@gmail.com>
2023-05-02 21:26:56 +0200

    fix: 404 page heading tag

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,9 +1,12 @@
 import { MainLayout } from '../../components/layouts/mainLayout.tsx';
+import { Heading } from '../../components/heading/heading.tsx';
 
 export const PageNotFound = () => {
 	return (
 		<MainLayout>
-			<h1>404 - Page not Found</h1>
+			<Heading tag='h1' size='large'>
+				404 - Page not Found
+			</Heading>
 		</MainLayout>
 	);
 };
